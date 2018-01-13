7caa37b7f426fabc5aab590a36cee08628903847
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-01-13 18:47:03 +0530

    ICFJ-317 - Increases bottom space of sticky and removes br space.

--- src/js/findings.js
+++ src/js/findings.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     var mode = window.innerWidth <= 500 ? 'mobile' : 'laptop';
 
     if (mode === 'laptop') {
-        $("#sticker").sticky({ topSpacing: 0, bottomSpacing: 320});
-        $('.related-articles-link').sticky({ topSpacing: 20, bottomSpacing: 320});
+        $("#sticker").sticky({ topSpacing: 0, bottomSpacing: 400});
+        $('.related-articles-link').sticky({ topSpacing: 20, bottomSpacing: 400});
         $('#cont-button').on('click', (e) => {
             $('#cont-button').css('display', 'none');
             document.getElementById('article').className = 'article-area';
