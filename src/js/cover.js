485f362d52082d325fce5cf5fd46f38f7c07206e
AashutoshSBhatt <aashutosh.bhatt@pykih.com>
2018-03-12 23:15:01 +0530

    PG-361 Adds sticky columns to the cover page

--- src/js/cover.js
+++ src/js/cover.js
@@ -12,10 +12,21 @@ ProtoGraph.initPage = function initPage() {
         digests_container,
         opinions_container,
         headerJSON = ProtoGraph.headerJSON,
-        streams = ProtoGraph.streams;
+        streams = ProtoGraph.streams,
+        sticky_sidebar_options;
 
-    // if (mode === 'laptop') {
-    // }
+    if (mode === 'laptop') {
+        sticky_sidebar_options = {
+            containerSelector: '#cover',
+            innerWrapperSelector: '.sidebar__inner',
+            resizeSensor: true,
+            bottomSpacing: 0
+        };
+        $('#originals_container').stickySidebar(sticky_sidebar_options);
+        $('#digests_container').stickySidebar(sticky_sidebar_options);
+        $('#feeds_container').stickySidebar(sticky_sidebar_options);
+        $('#opinions_container').stickySidebar(sticky_sidebar_options);
+    }
 
     if (mode === 'mobile') {
         //Set Tab counts and container counts.
