5765206d7c7353eb3f4e0429bb881636e79fd163
Nidhi <nidhi@pro.to>
2019-12-06 13:53:14 +0530

    - modified article twitter share

--- src/js/findings.js
+++ src/js/findings.js
@@ -8,7 +8,7 @@ function initFBTWShareLinks() {
         tw_share_url;
 
     fb_share_url = `http://www.facebook.com/sharer/sharer.php?u=${url}${fb_share ? '&description=' + encodeURI(fb_share) : ''}`;
-    tw_share_url = `http://twitter.com/share?url=${url}${tw_share ? '&text=' + encodeURI(tw_share) : ''}`;
+    tw_share_url = `http://twitter.com/share?url=${url}${tw_share ? '&text=' + encodeURIComponent(tw_share) : ''}`;
 
     if(document.getElementById('facebook-share-link')) {document.getElementById('facebook-share-link').href = fb_share_url;}
     if(document.getElementById('twitter-share-link')) {document.getElementById('twitter-share-link').href = tw_share_url;}
